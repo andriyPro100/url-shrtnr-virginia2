e9492508d1879df30b43ac60d2bec51055b9c12c
Artem Sevruk <32986253+tjomikode@users.noreply.github.com>
2021-03-19 19:04:33 +0200

    Update UrlRepositoryFakeImpl.java
    Added missing JavaDoc

--- src/main/java/edu/kpi/testcourse/storage/UrlRepositoryFakeImpl.java
+++ src/main/java/edu/kpi/testcourse/storage/UrlRepositoryFakeImpl.java
@@ -6,32 +6,42 @@ import java.util.HashMap;
 import java.util.List;
 import javax.annotation.Nullable;
 
-/**
+ /**
  * An in-memory fake implementation of {@link UrlRepository}.
  */
 public class UrlRepositoryFakeImpl implements UrlRepository {
   private final HashMap<String, UrlAlias> aliases = new HashMap<>();
-
+  
+ /**
+ * Adding of an URL alias into a HashMap storage. 
+ */
   @Override
   public void createUrlAlias(UrlAlias urlAlias) {
     if (aliases.containsKey(urlAlias.alias())) {
       throw new UrlRepository.AliasAlreadyExist();
     }
-
     aliases.put(urlAlias.alias(), urlAlias);
   }
 
+ /**
+ * Finding of an URL alias by alias name from the HashMap storage. 
+ */ 
   @Override
   public @Nullable UrlAlias findUrlAlias(String alias) {
     return aliases.get(alias);
   }
-
+  
+ /**
+ * Deleting of an URL alias from the HashMap storage. 
+ */
   @Override
   public void deleteUrlAlias(String email, String alias) {
     // TODO: We should implement it
     throw new UnsupportedOperationException();
   }
-
+ /**
+ * Getting a list of all URL aliases of certain user. 
+ */
   @Override
   public List<UrlAlias> getAllAliases(String username) {
     List<UrlAlias> aliasesList = new ArrayList<>();
