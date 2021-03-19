7cc4ae0fb153dd1a0ee5bf4e9aefe87a74b34ebd
Artem Sevruk <32986253+tjomikode@users.noreply.github.com>
2021-03-19 20:15:14 +0200

    Update UrlRepositoryFakeImpl.java
    Fixed typo

--- src/main/java/edu/kpi/testcourse/storage/UrlRepositoryFakeImpl.java
+++ src/main/java/edu/kpi/testcourse/storage/UrlRepositoryFakeImpl.java
@@ -15,43 +15,43 @@ public class UrlRepositoryFakeImpl implements UrlRepository {
  /**
  * Adding of an URL alias into a HashMap storage. 
  */
-  @Override
-  public void createUrlAlias(UrlAlias urlAlias) {
-    if (aliases.containsKey(urlAlias.alias())) {
-      throw new UrlRepository.AliasAlreadyExist();
-    }
-    aliases.put(urlAlias.alias(), urlAlias);
+@Override
+public void createUrlAlias(UrlAlias urlAlias) {
+  if (aliases.containsKey(urlAlias.alias())) {
+    throw new UrlRepository.AliasAlreadyExist();
   }
+  aliases.put(urlAlias.alias(), urlAlias);
+}
 
  /**
  * Finding of an URL alias by alias name from the HashMap storage. 
  */ 
-  @Override
-  public @Nullable UrlAlias findUrlAlias(String alias) {
-    return aliases.get(alias);
-  }
+@Override
+public @Nullable UrlAlias findUrlAlias(String alias) {
+  return aliases.get(alias);
+}
   
  /**
  * Deleting of an URL alias from the HashMap storage. 
  */
-  @Override
-  public void deleteUrlAlias(String email, String alias) {
-    // TODO: We should implement it
-    throw new UnsupportedOperationException();
-  }
+@Override
+public void deleteUrlAlias(String email, String alias) {
+  // TODO: We should implement it
+  throw new UnsupportedOperationException();
+}
  /**
  * Getting a list of all URL aliases of certain user. 
  */
-  @Override
-  public List<UrlAlias> getAllAliases(String username) {
-    List<UrlAlias> aliasesList = new ArrayList<>();
-    for (UrlAlias urlAlias : aliases.values()) {
-      if (urlAlias.email().equals(username)) {
-        aliasesList.add(urlAlias);
-      } else {
-        throw new PermissionDenied();
-      }
+@Override
+public List<UrlAlias> getAllAliases(String username) {
+  List<UrlAlias> aliasesList = new ArrayList<>();
+  for (UrlAlias urlAlias : aliases.values()) {
+    if (urlAlias.email().equals(username)) {
+      aliasesList.add(urlAlias);
+    } else {
+      throw new PermissionDenied();
     }
-    return aliasesList;
   }
+  return aliasesList;
+}
 }
